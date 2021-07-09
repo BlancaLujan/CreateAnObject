4e1d56ec94fbaa914eb2864368750312b78b78d9
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-07-09 14:13:09 -0600

    Create an object

---
+++ CreateAnObject/CreateAnObject/PastryBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CreateAnObject
+{
+    public class PastryBox
+    {
+
+        public string myBrand;
+
+        public int myItems;
+        public int myChocolateChipCookies;
+
+        public int myBrownies;
+        public int myOatmealCookies;
+    }
+}
+
---
+++ CreateAnObject/CreateAnObject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CreateAnObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PastryBox aNewClass = new PastryBox();
+
+            aNewClass.myBrand = "Pillsbury";
+            aNewClass.myItems = 100;
+            aNewClass.myChocolateChipCookies = 40;
+            aNewClass.myBrownies = 20;
+            aNewClass.myOatmealCookies = 40;
+
+            Console.WriteLine(aNewClass.myBrand);
+            Console.WriteLine(aNewClass.myItems);
+            Console.WriteLine(aNewClass.myChocolateChipCookies);
+            Console.WriteLine(aNewClass.myBrownies);
+            Console.WriteLine(aNewClass.myOatmealCookies);
+
+        }
+    }
+}
